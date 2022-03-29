eb774e263f33c9e66ceb45705ed72bfb602f6c6e
th368 <tom_hepworth@hotmail.co.uk>
2022-03-29 23:17:17 +0100

    add initial bot launch zone

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from discord.ext import commands
+
+# import our music cog
+from cogs.general_cog import Misc
+
+bot = commands.Bot(command_prefix='!')
+
+# remove the default help command so that we can write out own
+bot.remove_command('help')
+
+# register the class with the bot
+bot.add_cog(Misc(bot))
+
+# load in the token from within hiroku
+token = os.environ['TOKEN'] # use this if you're using the TOKEN within heroku
+# token = os.getenv('TOKEN') # use this if you're running from docker
+
+# with open("token.txt") as file:
+#     token = file.read()
+
+# power up our bot
+bot.run(token)
