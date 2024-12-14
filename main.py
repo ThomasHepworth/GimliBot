bc7c0c33d0d368dc023339ce309427b6aa867206
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2024-12-14 17:15:26 +0000

    feat(bot): orchestrate bot and add required cogs

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
-import discord
+import asyncio
+import logging
 import os
+
 from discord.ext import commands
 
-# import our music cog
-from cogs.general_cog import Misc
-from cogs.gifs_cog import Gifs
-from cogs.music_cog import Music
+from src.bot_intents import generate_music_bot_intents
+from src.cogs import HelperCommands, MusicCog
 
-bot = commands.Bot(command_prefix='!')
+# Setup Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# remove the default help command so that we can write out own
-bot.remove_command('help')
 
-# register the class with the bot
-bot.add_cog(Misc(bot))
-bot.add_cog(Gifs(bot))
-bot.add_cog(Music(bot))
+# Function to load bot cogs
+async def load_cogs(bot):
+    logger.info("Loading cogs...")
+    await bot.add_cog(MusicCog(bot))
+    logger.info("Cogs loaded successfully.")
 
-# load in the token from within hiroku
-token = os.environ['TOKEN'] # use this if you're using the TOKEN within heroku
-# token = os.getenv('TOKEN') # use this if you're running from docker
 
-# with open("token.txt") as file:
-#     token = file.read()
+# Main bot setup and run logic
+async def main():
+    logger.info("Initializing bot...")
+    bot = commands.Bot(
+        command_prefix="!",
+        help_command=HelperCommands(),
+        intents=generate_music_bot_intents(),
+    )
 
-# power up our bot
-bot.run(token)
+    # Load cogs
+    await load_cogs(bot)
+
+    # Retrieve bot token
+    token = os.getenv("TOKEN")
+    if not token:
+        logger.error("No bot token found. Please set the TOKEN environment variable.")
+        return
+
+    # Start the bot
+    try:
+        logger.info("Starting bot...")
+        await bot.start(token)
+    except Exception as e:
+        logger.error(f"Failed to start bot: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
